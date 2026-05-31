d64a32d8526fbfa9633ec87d2170223f8d6cbab0
Pedro Henrique <phhs@cin.ufpe.br>
2026-05-31 16:29:44 -0300

    Aritmetica, logica e deslocamentos (R-type)

--- src/pl_alu_ctrl.sv
+++ src/pl_alu_ctrl.sv
@@ -31,9 +31,13 @@ module pl_alu_ctrl (
             2'b10: begin                // R-type: decodificar Funct
                 case (Funct3)
                     3'h0: Operation = Funct7[5] ? 4'd02 : 4'd01; // SUB ou ADD
+                    3'h1: Operation = 4'd06;  // SLL
+                    3'h2: Operation = 4'd11;  // SLT
+                    3'h3: Operation = 4'd09;  // SLTU
+                    3'h4: Operation = 4'd03;  // XOR
+                    3'h5: Operation = Funct7[5] ? 4'd08 : 4'd07; // SRA ou SRL
                     3'h6: Operation = 4'd04;  // OR
                     3'h7: Operation = 4'd05;  // AND
-                    3'h2: Operation = 4'd11;  // SLT
                     default: Operation = 4'd01;
                 endcase
             end
