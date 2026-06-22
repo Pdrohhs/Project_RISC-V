23a8071dcf5086ed7588dddc21fba7ba8eb365c9
Rodrigo Silveira <rggs@cin.ufpe.br>
2026-06-22 18:52:33 -0300

    fix small issue on signext

---
+++ pl_sign_ext.sv
@@ -0,0 +1,65 @@
+// =============================================================================
+// pl_sign_ext.sv
+// Extensao de Sinal de Imediatos -- RV32I pipelined (P&H secao 4.4)
+//
+// Formatos suportados:
+//   LOAD (lw)    : imm[11:0]  = inst[31:20]
+//   I-TYPE(addi) : imm[11:0]  = inst[31:20]
+//   I-TYPE(srli) : imm[4:0] = inst[24:20]
+//   S-type (sw)  : imm[11:5]  = inst[31:25], imm[4:0] = inst[11:7]
+//   B-type (beq) : imm[12]=inst[31], imm[11]=inst[7], imm[10:5]=inst[30:25],
+//                  imm[4:1]=inst[11:8], imm[0]=0
+//   JALR  (1100111): imm[11:0]  = inst[31:20]  (mesmo formato I)
+//   JAL   (1101111): imm[20]=inst[31], imm[19:12]=inst[19:12],
+//                    imm[11]=inst[20], imm[10:1]=inst[30:21], imm[0]=0
+//	  LUI   (0110111):imm[31:12]
+//	  Auipc (0010111):imm[31:12]
+// =============================================================================
+
+`timescale 1ns / 1ps
+
+module pl_sign_ext (
+    input  logic [31:0] Instr,
+    output logic [31:0] ImmExt
+);
+
+    localparam LOAD   = 7'b0000011;
+	localparam I_TYPE = 7'b0010011;
+    localparam STORE  = 7'b0100011;
+    localparam BRANCH = 7'b1100011;
+    localparam JALR   = 7'b1100111;
+    localparam JAL    = 7'b1101111;
+	 localparam LUI    = 7'b0110111;
+	 localparam AUIPC  = 7'b0010111;
+
+    always_comb begin
+        case (Instr[6:0])
+            LOAD:   ImmExt = {{20{Instr[31]}}, Instr[31:20]};
+			
+			I_TYPE:begin
+				case(Instr[14:12])
+					3'b001: ImmExt = {{27{Instr[24]}}, Instr[24:20]};//slli
+					3'b101: ImmExt = {{27{Instr[24]}}, Instr[24:20]};//srli ou srai
+					default: ImmExt = {{20{Instr[31]}}, Instr[31:20]}; //addi,srti,ori,andi
+				endcase
+			end	
+
+            STORE:  ImmExt = {{20{Instr[31]}}, Instr[31:25], Instr[11:7]};
+
+            BRANCH: ImmExt = {{19{Instr[31]}}, Instr[31], Instr[7],
+                               Instr[30:25], Instr[11:8], 1'b0};
+
+            JALR:   ImmExt = {{20{Instr[31]}}, Instr[31:20]};
+
+            JAL:    ImmExt = {{11{Instr[31]}}, Instr[31], Instr[19:12],
+                               Instr[20], Instr[30:21], 1'b0};
+										 
+				LUI:   ImmExt = {Instr[31:12],12'b0};
+				
+				AUIPC: ImmExt = {Instr[31:12],12'b0};
+
+            default: ImmExt = 32'b0;
+        endcase
+    end
+
+endmodule
